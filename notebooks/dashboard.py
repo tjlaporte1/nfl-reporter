6f9f76903f7d699779648f8a6f974f7477f9ae99
tjlaporte1 <tjlaporte1@gmail.com>
2026-06-21 14:28:59 -0400

    dropdown test

--- notebooks/dashboard.py
+++ notebooks/dashboard.py
@@ -40,31 +40,52 @@ def _():
     import marimo as mo
     import polars as pl
 
-    return (pl,)
+    return mo, pl
 
 
 @app.cell
 def data_url(pl):
     BASE_DATA_URL = "https://raw.githubusercontent.com/tjlaporte1/nfl-reporter/main/data/"
 
-    teams_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"teams.parquet"}")
-    schedules_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"schedules.parquet"}")
-    team_stats_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"team-stats.parquet"}")
-    player_stats_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"player-stats.parquet"}")
-    rosters_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"rosters.parquet"}")
-    injuries_lf = pl.scan_parquet(f"{BASE_DATA_URL}{"injuries.parquet"}")
-    return team_stats_lf, teams_lf
-
-
-@app.cell
-def _(team_stats_lf):
-    team_stats_lf
-    return
+    teams_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'teams.parquet'}")
+    schedules_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'schedules.parquet'}")
+    team_stats_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'team-stats.parquet'}")
+    player_stats_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'player-stats.parquet'}")
+    rosters_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'rosters.parquet'}")
+    injuries_lf = pl.scan_parquet(f"{BASE_DATA_URL}{'injuries.parquet'}")
+    return (teams_lf,)
 
 
 @app.cell
 def _(teams_lf):
-    teams_lf.collect()
+    team_options = (
+        teams_lf.select("team_name")
+        .unique()
+        .sort("team_name")
+        .collect()
+        .get_column("team_name")  # ty:ignore[unresolved-attribute]
+        .to_list()
+    )  # ty:ignore[unresolved-attribute]
+    return (team_options,)
+
+
+@app.cell
+def _(mo, team_options):
+    team_dropdown = mo.ui.dropdown(
+        options=team_options,
+        value=team_options[0],
+        label="Select a team:",
+    )
+    team_dropdown
+    return (team_dropdown,)
+
+
+@app.cell
+def _(pl, team_dropdown, teams_lf):
+    selected_team = team_dropdown.value
+    filtered_df = teams_lf.filter(pl.col("team_name") == selected_team).collect()
+
+    filtered_df
     return
 
 
